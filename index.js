f0283aeddacc83bbca640cc96462bbd6e8693235
Alaaeldin <145039473+alaaeldin989@users.noreply.github.com>
2024-05-13 23:26:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// let btn = document.getElementById('btn');
+// let card = document.getElementById('card');
+// let info = document.querySelector('.info');
+// let info_card = document.querySelector('.info-body');
+
+
+// // btn.addEventListener('click', function(){
+// //     card.classList.toggle('active');
+// // })
+
+// info.addEventListener('click', function(){
+//     info_card.classList.toggle('active');
+// })
